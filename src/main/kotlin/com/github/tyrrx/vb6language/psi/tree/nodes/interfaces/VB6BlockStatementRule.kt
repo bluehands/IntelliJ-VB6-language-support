3e8dd29a176c817d6ffb7891b0e4f9ed157da307
David Retzlaff <david.r3tzlaff@gmail.com>
2022-03-03 23:08:55 +0100

    - Add block

--- src/main/kotlin/com/github/tyrrx/vb6language/psi/tree/nodes/interfaces/VB6BlockStatementRule.kt
+++ src/main/kotlin/com/github/tyrrx/vb6language/psi/tree/nodes/interfaces/VB6BlockStatementRule.kt
@@ -3,6 +3,6 @@ package com.github.tyrrx.vb6language.psi.tree.nodes.interfaces
 import com.github.tyrrx.vb6language.psi.tree.nodes.interfaces.base.VB6BlockStatement
 import com.intellij.psi.PsiElement
 
-interface VB6Block : PsiElement {
-    fun getBlockStatements(): List<VB6BlockStatement>
+interface VB6BlockStatementRule:PsiElement {
+    fun getStatement(): VB6BlockStatement
 }
