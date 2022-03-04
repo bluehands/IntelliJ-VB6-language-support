3284e5e44f37627b9ccf25e92631c8f0c9066cea
David Retzlaff <david.r3tzlaff@gmai.com>
2022-03-04 09:21:58 +0100

    - Add interfaces for block statements

---
+++ src/main/kotlin/com/github/tyrrx/vb6language/psi/tree/nodes/interfaces/blockStmt/VB6GoSubStmt.kt
@@ -0,0 +1,7 @@
+package com.github.tyrrx.vb6language.psi.tree.nodes.interfaces.blockStmt
+
+
+import com.github.tyrrx.vb6language.psi.tree.nodes.interfaces.base.VB6BlockStatement
+
+interface VB6GoSubStmt : VB6BlockStatement {
+}
