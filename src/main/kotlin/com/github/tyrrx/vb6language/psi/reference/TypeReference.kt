873f40b8679d3f2c1c062edbc654746b5e2818a7
David Retzlaff <david.r3tzlaff@gmail.com>
2022-03-23 11:11:17 +0100

    - Add References

---
+++ src/main/kotlin/com/github/tyrrx/vb6language/psi/reference/TypeReference.kt
@@ -0,0 +1,44 @@
+package com.github.tyrrx.vb6language.psi.reference
+
+import com.github.tyrrx.vb6language.psi.tree.definition.base.VB6ReferenceOwner
+import com.github.tyrrx.vb6language.psi.tree.definition.identifier.VB6Identifier
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+
+class TypeReference(
+    override val myElement: VB6ReferenceOwner,
+    override val referencingIdentifier: VB6Identifier,
+    override val textRange: TextRange
+) : VB6Reference {
+    override fun getElement(): PsiElement {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRangeInElement(): TextRange {
+        TODO("Not yet implemented")
+    }
+
+    override fun resolve(): PsiElement? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCanonicalText(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun handleElementRename(newElementName: String): PsiElement {
+        TODO("Not yet implemented")
+    }
+
+    override fun bindToElement(element: PsiElement): PsiElement {
+        TODO("Not yet implemented")
+    }
+
+    override fun isReferenceTo(element: PsiElement): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isSoft(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
