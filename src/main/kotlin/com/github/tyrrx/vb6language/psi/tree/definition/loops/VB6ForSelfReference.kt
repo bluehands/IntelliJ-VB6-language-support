85e8aa914a18b44790561514b6b6037f2bf3e4ff
David Retzlaff <david.r3tzlaff@gmail.com>
2022-03-31 13:03:43 +0200

    - Implement reference

--- src/main/kotlin/com/github/tyrrx/vb6language/psi/tree/definition/loops/VB6ForSelfReference.kt
+++ src/main/kotlin/com/github/tyrrx/vb6language/psi/tree/definition/loops/VB6ForSelfReference.kt
@@ -1,14 +1,17 @@
 package com.github.tyrrx.vb6language.psi.tree.definition.loops
 
 import com.github.tyrrx.vb6language.psi.language.IPsiNodeFactory
+import com.github.tyrrx.vb6language.psi.reference.SymbolReference
 import com.github.tyrrx.vb6language.psi.reference.VB6Reference
 import com.github.tyrrx.vb6language.psi.tree.definition.base.VB6PsiElement
 import com.github.tyrrx.vb6language.psi.tree.definition.base.VB6PsiNode
 import com.github.tyrrx.vb6language.psi.tree.definition.base.VB6ReferenceFactory
 import com.github.tyrrx.vb6language.psi.tree.definition.base.VB6ReferenceOwner
+import com.github.tyrrx.vb6language.psi.tree.definition.identifier.VB6Identifier
 import com.intellij.lang.ASTNode
 
-interface VB6ForSelfReference : VB6PsiElement, VB6ReferenceOwner {
+interface VB6ForSelfReference : VB6PsiElement, VB6ReferenceOwner, VB6ReferenceFactory {
+    val identifier: VB6Identifier?
 }
 
 class VB6ForSelfReferenceImpl(node: ASTNode) : VB6PsiNode(node), VB6ForSelfReference {
@@ -20,7 +23,17 @@ class VB6ForSelfReferenceImpl(node: ASTNode) : VB6PsiNode(node), VB6ForSelfRefer
     }
 
     override val referenceFactory: VB6ReferenceFactory?
-        get() = TODO("Not yet implemented")
+        get() = this
+
+    override fun createReference(): VB6Reference? {
+        return identifier?.let { SymbolReference(this, it, it.textRangeInParent) }
+    }
+
+    override val identifier: VB6Identifier?
+        get() = when (val id = firstChild) {
+            is VB6Identifier -> id
+            else -> null
+        }
 
     override fun getReference(): VB6Reference? {
         return referenceFactory?.createReference()
