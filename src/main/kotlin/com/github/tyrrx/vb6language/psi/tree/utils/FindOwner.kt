d6d4c87dba8e4cac4013b85ec34daf208cf8d7bf
David Retzlaff <david.r3tzlaff@gmail.com>
2022-04-02 21:35:30 +0200

    - Fix possible null

--- src/main/kotlin/com/github/tyrrx/vb6language/psi/tree/utils/FindOwner.kt
+++ src/main/kotlin/com/github/tyrrx/vb6language/psi/tree/utils/FindOwner.kt
@@ -5,22 +5,24 @@ import com.github.tyrrx.vb6language.psi.tree.definition.base.VB6ReferenceOwner
 import com.intellij.psi.PsiElement
 
 fun findOwnerRecursive(element: PsiElement): PsiElement {
-    return when(element) {
+    return when (element) {
         is VB6NamedElementOwner -> element
         is VB6ReferenceOwner -> element
         else -> findOwnerRecursive(element.parent)
     }
 }
 
-fun findIdentifierOwnerRecursive(element: PsiElement): VB6NamedElementOwner {
-    return when(element) {
+fun findIdentifierOwnerRecursive(element: PsiElement?): VB6NamedElementOwner? {
+    return when (element) {
+        null -> null
         is VB6NamedElementOwner -> element
         else -> findIdentifierOwnerRecursive(element.parent)
     }
 }
 
-fun findReferenceOwnerRecursive(element: PsiElement): VB6ReferenceOwner {
-    return when(element) {
+fun findReferenceOwnerRecursive(element: PsiElement?): VB6ReferenceOwner? {
+    return when (element) {
+        null -> null
         is VB6ReferenceOwner -> element
         else -> findReferenceOwnerRecursive(element.parent)
     }
